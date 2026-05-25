72dcd1518260dcc93855166f682474beb36f27e1
shawnpdriscoll <shawn.p.driscoll@gmail.com>
2026-05-25 07:32:32 -0400

    Fix duplicate HTTP response code handling

--- CognitiveAgeSite2/process-contact.php
+++ CognitiveAgeSite2/process-contact.php
@@ -82,3 +82,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo json_encode(['success' => false, 'message' => 'Method not allowed.']);
 }
 ?>
+
+    http_response_code(405);
+    echo json_encode(['success' => false, 'message' => 'Method not allowed.']);
+}
+?>
